5d8f003254652bd2d3849aafb6e1647a1ed060b9
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-12-09 15:26:09 -0300

    feat: metodo get para atributos privados

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,3 +1,11 @@
 export default abstract class Race {
   constructor(private _name: string, private _dexterity: number) {}
+  
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
 }
