b939583c5035d96121056522133b7cb5de0f9c7c
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-12-10 11:26:17 -0300

    feat: classe Elf

---
+++ src/Races/Elf.ts
@@ -0,0 +1,21 @@
+import Race from './Race';
+
+export default class Elf extends Race {
+  private _maxLifePoints: number;
+  private static _instancesCounter = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 99;
+    Elf._instancesCounter += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Elf._instancesCounter;
+  }
+}
