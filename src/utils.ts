8e9f498d9bf3ea8a6cf0334f9ef1f60c93e1ff67
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-12-10 16:21:04 -0300

    style: export default diretamente da função

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,5 @@
-function getRandomInt(min: number, max: number) {
+export default function getRandomInt(min: number, max: number) {
   const newMin = Math.ceil(min);
   const newMax = Math.floor(max);
   return Math.floor(Math.random() * (newMax - newMin)) + newMin;
 }
-
-export default getRandomInt;
